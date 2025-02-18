33ec57f4049c7b756b0145876b409a358056fcfb
lui1zz <luiz0921341@gmail.com>
2025-02-18 12:54:26 -0300

    package + getters e setters

---
+++ poo/src/com/mastersoft/Exame.java
@@ -0,0 +1,5 @@
+package com.mastersoft;
+
+public class Exame {
+    
+}
---
+++ poo/src/escola/Escola.java
@@ -0,0 +1,11 @@
+package escola;
+
+public class Escola {
+    public static void main(String[] args) {
+        Aluno felipe = new Aluno();
+        felipe.setNome("Felipe");
+        felipe.setIdade(8); 
+        felipe.setSexo("M");
+        System.out.println("O aluno "+ felipe.getNome() + " tem " + felipe.getIdade() + " anos e é do sexo "+felipe.getSexo()+".");
+    }
+}
